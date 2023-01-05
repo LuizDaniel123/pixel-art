8e935cf9b62fa66e6e984b09f84bed350587502d
LuizDaniel123 <luizespindola275@gmail.com>
2023-01-05 18:50:09 -0500

    style: criei o estilo do componente header

--- src/components/header/styled.js
+++ src/components/header/styled.js
@@ -1,6 +1,22 @@
 import styled from "styled-components";
 
 const Container = styled.header`
+    display: flex;
+    height: 5rem;
+    width: 50vw;
+
+    div {
+        background-color: #202020;
+        border-radius: 5px;
+        margin-right: 1rem;
+        height: 100%;
+        width: 100%;
+        
+    }
+
+    div:nth-child(6) {
+        margin-right: 0;
+    }
 `;
 
 export default Container;
