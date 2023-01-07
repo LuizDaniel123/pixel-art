abb36f5f0744ff87d6b1b35192c3370ada3032c5
LuizDaniel123 <luizespindola275@gmail.com>
2023-01-07 00:25:02 -0500

    feat: Adicionei uma lista de cores

---
+++ src/services/colors.js
@@ -0,0 +1,24 @@
+const colors = [
+    "#AEFFD8",
+    "#8AFFC1",
+    "#9F4A54",
+    "#33312E",
+    "#662C91",
+    "#EE6C4D",
+    "#17A398",
+    "#D1F0B1",
+    "#E0E2DB",
+    "#E6AF2E",
+    "#3D348B",
+    "#736B60",
+    "#4B4237",
+    "#C9C9EE",
+    "#5ADBFF",
+    "#FF495C",
+    "#46237A",
+    "#A31621",
+    "#7C7C7C",
+    "#FF36AB", 
+]; 
+
+export default colors;
