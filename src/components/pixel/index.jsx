5b1469bd8f645418ee08d5f9ccc1867c909f6d92
LuizDaniel123 <luizespindola275@gmail.com>
2023-01-07 00:23:43 -0500

    fix: Mudei a forma de adicionar uma quantidade grande de divs

--- src/components/pixel/index.jsx
+++ src/components/pixel/index.jsx
@@ -1,34 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Container from './styled'
 
 export default function Pixel() {
+  const createPixel = () => {
+    for (let i = 0; i < 100; i++) {
+      const main = document.querySelector('main');
+      const pixel = document.createElement('div');
+      main.appendChild(pixel);
+    }
+  }
+
+  useEffect(() => {
+    createPixel();
+  }, [])
+
   return (
     <Container>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
     </Container>
   )
 }
